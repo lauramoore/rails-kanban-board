80900ccb52634ffaeee29fee4691e683cf8d0c0b
Laura <laura.moore@rightbox.com>
2011-04-03 20:16:00 -0400

    adding jquery ui and sortable

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,6 @@ module RailsKanban
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.action_view.javascript_expansions[:defaults] = %w(jquery jquery-ui-custom.min)
   end
 end
